83ab205c1d576b0d6b87f051be27cfed34dd5a34
NelsonSantos7 <159441946+NelsonSantos7@users.noreply.github.com>
2024-03-19 11:48:26 +0000

    19/03/2024 Primeira navegacao concluída

--- app/src/main/java/com/example/gamevault/MainActivity.kt
+++ app/src/main/java/com/example/gamevault/MainActivity.kt
@@ -7,24 +7,25 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.gamevault.databinding.ActivityMainBinding
 import com.example.gamevault.login.Login
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 
-class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -32,41 +33,44 @@ class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        navController = findNavController(R.id.nav_host_fragment_content_main)
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_im_playing, R.id.nav_want_to_playing, R.id.nav_Ive_played, R.id.nav_AddGame
             ), drawerLayout
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        // Configuração do listener para os itens do menu de navegação
+        // Esta linha garante que o MainActivity escute por seleções de itens no NavigationView.
         navView.setNavigationItemSelectedListener(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val drawerLayout: DrawerLayout = binding.drawerLayout
         when (item.itemId) {
             R.id.nav_logout -> {
                 logout()
+                drawerLayout.closeDrawer(GravityCompat.START)
+                return true
             }
         }
-        // Fechar o drawer após a seleção do item
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
+        val handled = NavigationUI.onNavDestinationSelected(item, navController)
+        if (handled) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        return handled
     }
 
     private fun logout() {
